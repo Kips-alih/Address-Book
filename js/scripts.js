d4a0fdbce7c9e80550c7f49a96af69ced3d73e5a
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-13 20:04:20 +0300

    Use the fullName() method

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function Contact(first,last){
   this.lastName=last;
 }
 Contact.prototype.fullName=function(){
-  return this.firstName+ "" +this.lastName;
+  return this.firstName+ " " +this.lastName;
 };
 //user interface logic
 $(document).ready(function(){
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
     var newContact=new Contact(inputtedFirstName,inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>"+ newContact.firstName+"</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>"+ newContact.fullName()+"</span></li>");
 
     $(".contact").last().click(function(){
       $("#show-contact").show();
