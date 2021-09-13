8e15d8242817f6cd485d95fa1db9c5f1581ac6f1
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-13 19:59:18 +0300

    Create a fullName method on Contact prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function Contact(first,last){
   this.firstName=first;
   this.lastName=last;
 }
+Contact.prototype.fullName=function(){
+  return this.firstName+ "" +this.lastName;
+};
 //user interface logic
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
