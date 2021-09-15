da4b5ef716ed5ddbd4b094e665a08ed8dbcb1e1b
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-15 13:45:57 +0300

    Clear address fields after form submission

--- js/scripts.js
+++ js/scripts.js
@@ -62,6 +62,12 @@ $(document).ready(function(){
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-county").val("");
+
+
+
 
   });
 });
