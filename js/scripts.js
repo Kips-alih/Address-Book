edd1d9dd9611c975f3fc8cd6c2aaf57e7a2f9d13
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-09-16 10:17:38 +0300

    Add address type

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,18 @@ function Contact(first,last){
   this.lastName=last;
   this.addresses=[];
 }
-function Address(street,city,county){
+function Address(street,city,county,type){
   this.street=street;
   this.city=city;
   this.county=county;
+  this.addressType=type;
 }
 
 Contact.prototype.fullName=function(){
   return this.firstName+ " " +this.lastName;
 };
 Address.prototype.fullAddress=function(){
-  return this.street + ", "+ this.city + ", " + this.county;
+  return this.street + ", "+ this.city + ", " + this.county +", "+ this.addressType;
 };
 //user interface logic
 function resetFields(){
@@ -39,6 +40,15 @@ $(document).ready(function(){
                                     '<label for="new-county">County</label>'+
                                     '<input type="text" class="form-control new-county">'+
                                   '</div>'+
+                                  '<div class="form-group">'+
+                                    '<label for="address-type">Address Type</label>'+
+                                    '<select class="form-control address-type">'+
+                                      '<option selected disabled value="-"> Select type..</option>'+
+                                      '<option>Home</option>'+
+                                      '<option>Business</option>'+
+                                      '<option>Other</option>'+
+                                    '</select>'+
+                                  '</div>'+
                                 '</div>');
   });
 
@@ -54,7 +64,8 @@ $(document).ready(function(){
       var inputtedStreet=$(this).find("input.new-street").val();
       var inputtedCity=$(this).find("input.new-city").val();
       var inputtedCounty=$(this).find("input.new-county").val();
-      var newAddress= new Address(inputtedStreet,inputtedCity,inputtedCounty)
+      var selectedAddressType=$(this).find("select.address-type").val();
+      var newAddress= new Address(inputtedStreet,inputtedCity,inputtedCounty,selectedAddressType)
       newContact.addresses.push(newAddress)
     });
 
